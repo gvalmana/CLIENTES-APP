176e548087ae6b25ca1f8b5fb03dfbafec7dc0b2
goku1988 <gustavo.shotokai@gmail.com>
2020-08-20 14:24:01 -0400

    First commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Bienvenido a tu proyecto.js App"/>
   </div>
 </template>
 
