6bc5a7f12bedf6aba17d81bae7b36e6035a1bfcc
goku1988 <gustavo.shotokai@gmail.com>
2020-08-21 11:14:50 -0400

    Fist commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Bienvenido a tu proyecto.js App"/>
+    <Hola msg="Bienvenido a tu proyecto.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Hola from '@/components/Hola.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Hola
   }
 }
 </script>
